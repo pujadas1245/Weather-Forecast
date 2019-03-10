84c8080663d6b9caed40449d16f68afd9886180d
Puja Das <pujadas1245@gmail.com>
2019-03-10 22:15:50 +0530

    show weather forecast

--- js/index.js
+++ js/index.js
@@ -1,63 +1,69 @@
-const apiKey="9c3fe5394573fc5670e07c55ec77eba6";
+const apiKey = "9c3fe5394573fc5670e07c55ec77eba6";
 
 //getting all the required elements
-let input= document.querySelector("#search-txt");
-let search= document.querySelector("#search-btn");
-let displayCity= document.querySelector("#city-name");
-let cityIcon= document.querySelector("#icon");
-let temperature= document.querySelector("#temp");
-let humidity= document.querySelector("#humidity-div");
+let input = document.querySelector("#search-txt");
+let search = document.querySelector("#search-btn");
+let displayCity = document.querySelector("#city-name");
+let cityIcon = document.querySelector("#icon");
+let temperature = document.querySelector("#temp");
+let humidity = document.querySelector("#humidity-div");
 
 //by pressing enter, trigger a function
-input.addEventListener('keyup', function(e){
+input.addEventListener('keyup', function (e) {
     //console.log(e);
-    e.key== "Enter"?fetchWeatherDetails():false;
+    e.key == "Enter" ? fetchWeatherDetails() : false;
 })
 //clicks on search icon
-search.addEventListener('click', function(e){
+search.addEventListener('click', function (e) {
     //console.log(e);
     fetchWeatherDetails();
 })
 
 //call http request function to get data
-function fetchWeatherDetails(){
-    if(input.value !=''){
-        let url="https://api.openweathermap.org/data/2.5/weather?q="+input.value+"&appid="+apiKey;
+function fetchWeatherDetails() {
+    if (input.value != '') {
+        let url = "https://api.openweathermap.org/data/2.5/weather?q=" + input.value + "&appid=" + apiKey;
         //console.log(url);
         httpRequestAsync(url);
     }
 }
 
+
 //request & response
-function httpRequestAsync(){
+function httpRequestAsync() {
     //browser built-in object XMLHttpRequest to exchange data
-let xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange=function(){
-    //when response is ready
-    if(this.readyState== 4 && this.state==200){
-        console.log(xhttp.responseText);
-        action(xhttp.responseText);
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4){
+            if(this.status == 200) {
+            action(this.responseText);
+            }
+            else{
+                alert((JSON.parse(this.responseText)).message);
+                input.value="";
+            }
+        }
     }
-};
-//using arguments object to get arguments
-let url= arguments[0];
-//console.log(url);
-xhttp.open("GET", url, true);
-xhttp.send();
+    //using arguments object to get arguments
+    let url= arguments[0];
+    xhttp.open("GET", url, true);
+    xhttp.send();
 }
 
 //show city name, temperature, humidity
-function action(response){
+function action(response) {
     //convert to json object from string
-    let objResponse= JSON.parse(response);
-    console.log(objResponse);
-    displayCity.textContent=objResponse.name;
-    cityIcon.src="http://openweathermap.org/img/w/"+objResponse.weather[0].icon+".png";
+    let objResponse = JSON.parse(response);
+        displayCity.textContent = objResponse.name;
+        cityIcon.src = "http://openweathermap.org/img/w/" + objResponse.weather[0].icon + ".png";
     
-    humidity.textContent=objResponse.main.humidity+"%";
-    //default temperature's unit kelvin(response)
-    //Tc=Tk-273
-    temperature.textContent=parseInt(objResponse.main.temp-273)+"°";
+        humidity.textContent = objResponse.main.humidity + "%";
+        //default temperature's unit kelvin(response)
+        //Tc=Tk-273
+        temperature.textContent = parseInt(objResponse.main.temp - 273) + "°";
 
 }
+
+
+
 
